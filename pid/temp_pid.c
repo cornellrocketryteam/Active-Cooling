878ff10f7dc8b557ad868d9e72d6ceebc1fc1f85
Nandita Nagarajan <nslnmusic@gmail.com>
2025-04-14 14:02:14 -0400

    added main method bc I lost/forgot it lol

--- pid/temp_pid.c
+++ pid/temp_pid.c
@@ -46,7 +46,8 @@ float measured_temp = 30;
 
 
 // Variable to hold PWM slice number
-uint slice_num ;
+uint slice_num_1 ;
+uint slice_num_2 ;
 
 // PWM duty cycle
 volatile int control ;
@@ -73,7 +74,8 @@ void on_pwm_wrap() {
     // Update duty cycle
     if (control!=old_control) {
         old_control = control ;
-        pwm_set_chan_level(slice_num, PWM_CHAN_A, control);
+        pwm_set_chan_level(slice_num_1, PWM_CHAN_A, control);
+        pwm_set_chan_level(slice_num_2, PWM_CHAN_B, control);
     }
     
     // Read the temp sensor
@@ -90,4 +92,65 @@ void on_pwm_wrap() {
     else if (control > 2500){
         control = 2500;
     }
+}
+int main() {
+
+    // Initialize stdio
+    stdio_init_all();
+
+    ////////////////////////////////////////////////////////////////////////
+    ///////////////////////// I2C CONFIGURATION ////////////////////////////
+    // i2c_init(I2C_CHAN, I2C_BAUD_RATE) ;
+    // gpio_set_function(SDA_PIN, GPIO_FUNC_I2C) ;
+    // gpio_set_function(SCL_PIN, GPIO_FUNC_I2C) ;
+
+
+    ////////////////////////////////////////////////////////////////////////
+    ///////////////////////// PWM CONFIGURATION ////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+    // Tell GPIO PWM_OUT that it is allocated to the PWM
+    gpio_set_function(PWM_OUT_1, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_OUT_2, GPIO_FUNC_PWM);
+
+    // Find out which PWM slice is connected to GPIO PWM_OUT (it's slice 2)
+    slice_num_1 = pwm_gpio_to_slice_num(PWM_OUT_1);
+    slice_num_2 = pwm_gpio_to_slice_num(PWM_OUT_2);
+
+    // Mask our slice's IRQ output into the PWM block's single interrupt line,
+    // and register our interrupt handler
+    pwm_clear_irq(slice_num_1);
+    pwm_clear_irq(slice_num_2);
+    pwm_set_irq_enabled(slice_num_1, true);
+    pwm_set_irq_enabled(slice_num_2, true);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, on_pwm_wrap);
+    irq_set_enabled(PWM_IRQ_WRAP, true);
+
+    // This section configures the period of the PWM signals
+    pwm_set_wrap(slice_num_1, WRAPVAL) ;
+    pwm_set_wrap(slice_num_2, WRAPVAL);
+    pwm_set_clkdiv(slice_num_1, CLKDIV) ;
+    pwm_set_clkdiv(slice_num_2, CLKDIV) ; 
+
+    pwm_set_output_polarity(slice_num_1, 1, 1);
+    pwm_set_output_polarity(slice_num_2, 1, 1);
+
+    // This sets duty cycle
+    pwm_set_chan_level(slice_num_1, PWM_CHAN_A, 3125);
+    pwm_set_chan_level(slice_num_2, PWM_CHAN_A, 3125);
+
+    // Start the channel
+    pwm_set_mask_enabled((1u << slice_num_1));
+    pwm_set_mask_enabled((1u << slice_num_2));
+
+    ////////////////////////////////////////////////////////////////////////
+    ///////////////////////////// ROCK AND ROLL ////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+    // // start core 1 
+    // multicore_reset_core1();
+    // multicore_launch_core1(core1_entry);
+
+    // // start core 0
+    // pt_add_thread(protothread_serial) ;
+    // pt_schedule_start ;
+
 }
